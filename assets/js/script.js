5e21e42d0caa19a1c2100e1d53483197f3b53f7f
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-04-23 10:57:54 -0700

    updated for save data functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,70 @@
+//handle for currentday  display
 var currentDayEl = $("#currentDay");
+//get handle for save button
+var saveButtonEl = $(".saveBtn");
 
+//get current hour
+const currentTime = moment().hour();
 date = moment().format("dddd, MMMM Do YYYY");
 currentDayEl.text(date);
-// function setTime() {
-//   // Sets interval in variable
-//   var timerInterval = setInterval(function () {
-//     // dateTime = moment().format("MMMM Do YYYY at h:mm:ss");
 
-//   }, 1000);
+//change color of textbox according to current time
+changeColor();
+
+//changecolor when hour changed
+setInterval(() => {
+  changeColor();
+}, 60000);
+
+//function to change color
+function changeColor() {
+  var textboxArray = $(".description");
+  $.each(textboxArray, function (indexNum, value) {
+    //console.log($(this).attr("id"));
+    var schedulerHour = $(this).attr("id");
+    console.log("schedular time", schedulerHour);
+    console.log("current time", currentTime);
+
+    if (currentTime > schedulerHour) {
+      //assign class past
+      $(value).addClass("past");
+    } else if (currentTime == schedulerHour) {
+      //assign class present
+      $(value).addClass("present");
+    } else if (currentTime < schedulerHour) {
+      //assign class future
+      $(value).addClass("future");
+    }
+  });
+}
+
+//function to save data
+
+function saveData(event) {
+  //get data from user when saved
+  var id = $(event.target).closest(".row").find(".description").attr("id");
+  var text = $(event.target).closest(".row").find(".description").val();
+
+  //get local data if there is
+  var data = JSON.parse(localStorage.getItem("data")) || [];
+
+  //add user data to local data
+  var newData = {
+    id: id,
+    text: text,
+  };
+  data.push(newData);
+
+  // write back to local data
+  localStorage.setItem("data", JSON.stringify(data));
+}
+
+saveButtonEl.on("click", saveData);
+
+// var data = JSON.parse(localStorage.getItem("data")) || [];
+// if (data.length > 0) {
+//   data.forEach((datum) => {
+//     var query = `[id="${datum.id}"]`; //"[id='" +datum.id+ "']"
+//     $(query).val(datum.text);
+//   });
 // }
-// setTime();
